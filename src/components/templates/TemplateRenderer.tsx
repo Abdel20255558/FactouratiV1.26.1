32f58638c94ac9cac99755a72ac0665e86425c22
Abdel20255558 <abdelidrissi25@gmail.com>
2026-03-30 23:25:02 +0100

    Update TemplateRenderer.tsx

--- src/components/templates/TemplateRenderer.tsx
+++ src/components/templates/TemplateRenderer.tsx
@@ -5,6 +5,9 @@ import Template2Modern from './Template2Modern';
 import Template3Minimal from './Template3Minimal';
 import Template4Corporate from './Template4Corporate';
 import Template5Premium from './Template5Premium';
+import Template6Executive from './Template6Executive';
+import Template7Atlas from './Template7Atlas';
+import Template8Prestige from './Template8Prestige';
 
 interface TemplateRendererProps {
   templateId: string;
@@ -25,6 +28,12 @@ export default function TemplateRenderer({ templateId, data, type, includeSignat
       return <Template4Corporate data={data} type={type} includeSignature={includeSignature} />;
     case 'template5':
       return <Template5Premium data={data} type={type} includeSignature={includeSignature} />;
+    case 'template6':
+      return <Template6Executive data={data} type={type} includeSignature={includeSignature} />;
+    case 'template7':
+      return <Template7Atlas data={data} type={type} includeSignature={includeSignature} />;
+    case 'template8':
+      return <Template8Prestige data={data} type={type} includeSignature={includeSignature} />;
     default:
       return <Template1Classic data={data} type={type} includeSignature={includeSignature} />;
   }
