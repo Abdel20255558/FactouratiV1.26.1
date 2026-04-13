fe3df3a5775d1cb99a8952f8002482b7acc67629
Abdel20255558 <abdelidrissi25@gmail.com>
2026-04-13 21:18:17 +0100

    Update TemplateRenderer.tsx

--- src/components/templates/TemplateRenderer.tsx
+++ src/components/templates/TemplateRenderer.tsx
@@ -8,33 +8,35 @@ import Template5Premium from './Template5Premium';
 import Template6Executive from './Template6Executive';
 import Template7Atlas from './Template7Atlas';
 import Template8Prestige from './Template8Prestige';
+import type { TemplateCompany } from './Template1Classic';
 
 interface TemplateRendererProps {
   templateId: string;
   data: Invoice | Quote;
   type: 'invoice' | 'quote';
   includeSignature?: boolean;
+  companyOverride?: TemplateCompany;
 }
 
-export default function TemplateRenderer({ templateId, data, type, includeSignature = false }: TemplateRendererProps) {
+export default function TemplateRenderer({ templateId, data, type, includeSignature = false, companyOverride }: TemplateRendererProps) {
   switch (templateId) {
     case 'template1':
-      return <Template1Classic data={data} type={type} includeSignature={includeSignature} />;
+      return <Template1Classic data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
     case 'template2':
-      return <Template2Modern data={data} type={type} includeSignature={includeSignature} />;
+      return <Template2Modern data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
     case 'template3':
-      return <Template3Minimal data={data} type={type} includeSignature={includeSignature} />;
+      return <Template3Minimal data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
     case 'template4':
-      return <Template4Corporate data={data} type={type} includeSignature={includeSignature} />;
+      return <Template4Corporate data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
     case 'template5':
-      return <Template5Premium data={data} type={type} includeSignature={includeSignature} />;
+      return <Template5Premium data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
     case 'template6':
-      return <Template6Executive data={data} type={type} includeSignature={includeSignature} />;
+      return <Template6Executive data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
     case 'template7':
-      return <Template7Atlas data={data} type={type} includeSignature={includeSignature} />;
+      return <Template7Atlas data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
     case 'template8':
-      return <Template8Prestige data={data} type={type} includeSignature={includeSignature} />;
+      return <Template8Prestige data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
     default:
-      return <Template1Classic data={data} type={type} includeSignature={includeSignature} />;
+      return <Template1Classic data={data} type={type} includeSignature={includeSignature} companyOverride={companyOverride} />;
   }
 }
